246c30d23680714386c6f63ae0bdb562b7232a77
KatzMatz <katz.matz.dev@gmail.com>
2024-11-09 08:37:25 +0900

    define constant calcuation

--- common/matrix_util.go
+++ common/matrix_util.go
@@ -1,6 +1,8 @@
 package common
 
-import "gonum.org/v1/gonum/mat"
+import (
+	"gonum.org/v1/gonum/mat"
+)
 
 // m x n . n x l -> m x l
 func Dot(a, b mat.Matrix) mat.Matrix {
@@ -19,6 +21,36 @@ func Add(a, b mat.Matrix) mat.Matrix {
 	c.Add(a, b)
 
 	return c
+}
+
+func SubConstant(a mat.Matrix, v float64) mat.Matrix {
+	row, col := a.Dims()
+	constants := mat.NewDense(row, col, RepeatSlice(v, (row*col)))
+
+	b := mat.NewDense(row, col, nil)
+	b.Sub(a, constants)
+
+	return b
+}
+
+func DivConstant(x mat.Matrix, v float64) mat.Matrix {
+	row, col := x.Dims()
+	constants := mat.NewDense(row, col, RepeatSlice(v, (row*col)))
+
+	result := mat.NewDense(row, col, nil)
+	result.DivElem(x, constants)
+
+	return result
+}
+
+func RepeatSlice[T int | float32 | float64](v T, n int) []T {
+	s := []T{}
+
+	for range n {
+		s = append(s, v)
+	}
+
+	return s
 }
 
 func ApplyFunction(x mat.Matrix, f func(v float64) float64) mat.Matrix {
@@ -41,4 +73,28 @@ func ARange(start, stop, step float64) []float64 {
 func ARangeVector(start, stop, step float64) mat.Matrix {
 	s := ARange(start, stop, step)
 	return mat.NewVecDense(len(s), s)
+}
+
+func maxAtRow(x mat.Matrix, row int) float64 {
+	_, c := x.Dims()
+
+	result := x.At(row, 0)
+	for idx := range c {
+		if result < x.At(row, idx) {
+			result = x.At(row, idx)
+		}
+	}
+
+	return result
+}
+
+func SumRow(x mat.Matrix, row int) float64 {
+	_, col := x.Dims()
+
+	sum := 0.0
+	for idx := range col {
+		sum += x.At(row, idx)
+	}
+
+	return sum
 }
