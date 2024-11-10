7fba52d1ed4dbbb517a3af1db12f56a46731ce31
KatzMatz <katz.matz.dev@gmail.com>
2024-11-10 15:07:34 +0900

    add softmax function for matrix

--- common/functions.go
+++ common/functions.go
@@ -37,3 +37,60 @@ func SoftMax(x mat.Matrix) mat.Matrix {
 
 	return result
 }
+
+func SoftMaxMatrix(x mat.Matrix) mat.Matrix {
+	row, col := x.Dims()
+
+	floats := []float64{}
+
+	for idx := range row {
+		c := maxAtRow(x, idx)
+		expArgs := applyFunctionWithArg(GetRow(x, idx), sub, c)
+		exps := applyFunction(expArgs, math.Exp)
+		sumExps := fold(exps, func(a, b float64) float64 { return a + b }, 0)
+
+		rowSoftMax := applyFunctionWithArg(exps, div, sumExps)
+		floats = append(floats, rowSoftMax...)
+	}
+
+	result := mat.NewDense(row, col, floats)
+
+	return result
+}
+
+func fold[T int | float64](s []T, f func(a, b T) T, initial T) T {
+	result := initial
+	for _, v := range s {
+		result += f(result, v)
+	}
+
+	return result
+}
+
+func sub(a, b float64) float64 {
+	return (a - b)
+}
+
+func div(a, b float64) float64 {
+	return (a / b)
+}
+
+func applyFunction(s []float64, f func(v float64) float64) []float64 {
+	result := []float64{}
+
+	for _, value := range s {
+		result = append(result, f(value))
+	}
+
+	return result
+}
+
+func applyFunctionWithArg(s []float64, f func(v, arg float64) float64, c float64) []float64 {
+	result := []float64{}
+
+	for _, value := range s {
+		result = append(result, f(value, c))
+	}
+
+	return result
+}
